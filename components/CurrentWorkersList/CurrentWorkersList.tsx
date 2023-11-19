aabcf9340dfecc6815588cd15416d8711c208f40
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-19 18:13:50 +0100

    Changed html markup and css to the CurrentWorkers list

--- components/CurrentWorkersList/CurrentWorkersList.tsx
+++ components/CurrentWorkersList/CurrentWorkersList.tsx
@@ -6,79 +6,113 @@ import useWorker from "@/hooks/useWorker";
 import { useAppSelector } from "@/store/hooks";
 
 type WorkersProps = {
-  workers: WorkersFirebaseType[];
+	workers: WorkersFirebaseType[];
 };
 
 const CurrentWorkersList = (props: WorkersProps) => {
-  const [, , , removeWorkerFromFirebase] = useWorker();
-  const [openDialog, setOpenDialog] = useState(false);
-  const [workerInfo, setWorkerInfo] = useState<WorkersFirebaseType>({
-    id: "",
-    name: "",
-    email: "",
-    uid: "",
-  });
-  const { value } = useAppSelector((state) => state.dashboardNav);
+	const [, , , removeWorkerFromFirebase] = useWorker();
+	const [openDialog, setOpenDialog] = useState(false);
+	const [workerInfo, setWorkerInfo] = useState<WorkersFirebaseType>({
+		id: "",
+		name: "",
+		email: "",
+		uid: "",
+	});
+	const { value } = useAppSelector((state) => state.dashboardNav);
 
-  const removeWorkerHandler = (workerId: string, workerUid: string) => {
-    removeWorkerFromFirebase(workerId, workerUid);
-    setOpenDialog(false);
-  };
+	const removeWorkerHandler = (workerId: string, workerUid: string) => {
+		removeWorkerFromFirebase(workerId, workerUid);
+		setOpenDialog(false);
+	};
 
-  const openDialogHandler = (worker: WorkersFirebaseType) => {
-    setOpenDialog(true);
-    setWorkerInfo({
-      id: worker.id,
-      name: worker.name,
-      email: worker.email,
-      uid: worker.uid,
-    });
-  };
+	const openDialogHandler = (worker: WorkersFirebaseType) => {
+		setOpenDialog(true);
+		setWorkerInfo({
+			id: worker.id,
+			name: worker.name,
+			email: worker.email,
+			uid: worker.uid,
+		});
+	};
 
-  return (
-    <>
-      {value === "Workers" ? (
-        <div className={styles.container}>
-          {props.workers.map((worker: WorkersFirebaseType, index: number) => (
-            <div className={styles.worker} key={index}>
-              {worker.name}
-              <button
-                title="Remove worker"
-                className={styles.btn}
-                onClick={() => openDialogHandler(worker)}
-              >
-                <svg className={styles.closeIcon}>
-                  <use xlinkHref={"./svg/sprite.svg#close"}></use>
-                </svg>
-              </button>
-            </div>
-          ))}
-          {openDialog ? (
-            <div className={styles.dialog}>
-              <p>
-                Are you sure you want to delete worker:{" "}
-                <span className="">{workerInfo.name}</span>
-              </p>
-              <div>
-                <button
-                  onClick={() =>
-                    removeWorkerHandler(workerInfo.id, workerInfo.uid)
-                  }
-                >
-                  Yes I&apos;m sure
-                </button>
-                <button onClick={() => setOpenDialog(false)}>No</button>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+	return (
+		<>
+			{value === "Workers" ? (
+				<div className={styles.container}>
+					<table className={styles.table}>
+						<thead>
+							<tr>
+								<th>Worker</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							{props.workers.map(
+								(
+									worker: WorkersFirebaseType,
+									index: number
+								) => (
+									<tr key={index}>
+										{/* <div className={styles.worker}> */}
+										<td className={styles.worker}>
+											{worker.name}
+										</td>
+										<td className={styles.action}>
+											<button
+												title="Remove worker"
+												className={styles.btn}
+												onClick={() =>
+													openDialogHandler(worker)
+												}
+											>
+												<svg
+													className={styles.closeIcon}
+												>
+													<use
+														xlinkHref={
+															"./svg/sprite.svg#close"
+														}
+													></use>
+												</svg>
+											</button>
+										</td>
+										{/* </div> */}
+									</tr>
+								)
+							)}
+						</tbody>
+					</table>
+					{openDialog ? (
+						<div className={styles.dialog}>
+							<p>
+								Are you sure you want to delete worker:{" "}
+								<span className="">{workerInfo.name}</span>
+							</p>
+							<div>
+								<button
+									onClick={() =>
+										removeWorkerHandler(
+											workerInfo.id,
+											workerInfo.uid
+										)
+									}
+								>
+									Yes I&apos;m sure
+								</button>
+								<button onClick={() => setOpenDialog(false)}>
+									No
+								</button>
+							</div>
+						</div>
+					) : (
+						""
+					)}
+				</div>
+			) : (
+				""
+			)}
+		</>
+	);
 };
 
 export default CurrentWorkersList;
