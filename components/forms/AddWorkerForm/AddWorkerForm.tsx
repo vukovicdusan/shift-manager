c7413f8b9bdfb6122c64a21d6f9557e8ac52265f
Picvajzmen <picvajzmen@gmail.com>
2023-11-21 14:48:21 +0100

    Layout change for add worker form

--- components/forms/AddWorkerForm/AddWorkerForm.tsx
+++ components/forms/AddWorkerForm/AddWorkerForm.tsx
@@ -18,53 +18,57 @@ const AddWorkerForm = () => {
           }
           className={styles.form}
         >
-          <div className={styles.stack}>
-            <div className={styles.inputWrapperColumn}>
-              <label htmlFor="username">Name</label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                autoCapitalize="off"
-                autoCorrect="off"
-                required
-                onChange={inputHandler}
-              />
+          <div className={styles.flex}>
+            <div className={styles.stack}>
+              <div className={styles.inputWrapperColumn}>
+                <label htmlFor="username">Name</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  autoCapitalize="off"
+                  autoCorrect="off"
+                  required
+                  onChange={inputHandler}
+                />
+              </div>
+              <div className={styles.inputWrapperColumn}>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  autoCapitalize="off"
+                  autoCorrect="off"
+                  required
+                  pattern="[^@]+@[^\.]+\..+"
+                  onChange={inputHandler}
+                  title="Please enter a real mail"
+                />
+              </div>
             </div>
-            <div className={styles.inputWrapperColumn}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                autoCapitalize="off"
-                autoCorrect="off"
-                required
-                pattern="[^@]+@[^\.]+\..+"
-                onChange={inputHandler}
-                title="Please enter a real mail"
-              />
-            </div>
-            <div className={styles.inputWrapperColumn}>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                onChange={inputHandler}
-                required
-              />
-            </div>
-            <div className={styles.inputWrapperColumn}>
-              <label htmlFor="repeatPassword">Repeat Password</label>
-              <input
-                className={passwordError ? styles.error : ""}
-                type="password"
-                id="repeatPassword"
-                name="repeatPassword"
-                onChange={inputHandler}
-                required
-              />
+            <div className={styles.stack}>
+              <div className={styles.inputWrapperColumn}>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={inputHandler}
+                  required
+                />
+              </div>
+              <div className={styles.inputWrapperColumn}>
+                <label htmlFor="repeatPassword">Repeat Password</label>
+                <input
+                  className={passwordError ? styles.error : ""}
+                  type="password"
+                  id="repeatPassword"
+                  name="repeatPassword"
+                  onChange={inputHandler}
+                  required
+                />
+              </div>
             </div>
           </div>
           <button type="submit">Add Worker</button>
