57e584b273286bcd262180f34f6f09846f06bc5b
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-23 16:34:20 +0100

    Changed font to titilium

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,46 @@
 import ProviderComponent from "@/store/provider";
 import "../styles/globals.css";
-import { Inter } from "next/font/google";
+import { Titillium_Web } from "next/font/google";
 import Region from "@/components/Region";
 import Wrapper from "@/components/Wrapper";
 import Header from "@/components/Header/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const titilium = Titillium_Web({
+	weight: ["300", "400", "700"],
+	subsets: ["latin"],
+	display: "swap",
+});
 
 export const metadata = {
-  title: "We Know Shifts",
-  description: "We Know Shift Manager",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "./favicon.ico",
-    },
-  ],
+	title: "We Know Shifts",
+	description: "We Know Shift Manager",
+	icons: [
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			url: "./favicon.ico",
+		},
+	],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <ProviderComponent>
-        <body className={inter.className}>
-          <Header></Header>
-          <Region>
-            <Wrapper>
-              <main>{children}</main>
-            </Wrapper>
-          </Region>
-        </body>
-      </ProviderComponent>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<ProviderComponent>
+				<body className={titilium.className}>
+					<Header></Header>
+					<Region>
+						<Wrapper>
+							<main>{children}</main>
+						</Wrapper>
+					</Region>
+				</body>
+			</ProviderComponent>
+		</html>
+	);
 }
