f7aeb774c8dc5fa9f55fede6fc2360c9d9e8f046
Picvajzmen <picvajzmen@gmail.com>
2023-12-05 14:50:42 +0100

    Added overtime info to calendar element

--- helpers/renderEventContent.tsx
+++ helpers/renderEventContent.tsx
@@ -2,6 +2,7 @@ import { EventContentArg } from "@fullcalendar/core";
 
 export const renderEventContent = (shiftInfo: EventContentArg) => {
   const isDay = shiftInfo.event.classNames[0] === "day" ? "day" : "night";
+  // console.log("extended props", shiftInfo.event.extendedProps.overtime.hours);
   return (
     <>
       <div className="flex">
@@ -9,7 +10,22 @@ export const renderEventContent = (shiftInfo: EventContentArg) => {
           <use xlinkHref={"./svg/sprite.svg#" + isDay}></use>
         </svg>
         {/* <p>{shiftInfo.timeText}</p> */}
-        <p>{shiftInfo.event.title}</p>
+        <div className="shift-info flex space-between">
+          <p>{shiftInfo.event.title}</p>
+          {shiftInfo.event.extendedProps.overtime && (
+            <span
+              className={
+                shiftInfo.event.extendedProps.overtime.authorized
+                  ? "authorized"
+                  : "unauthorized"
+              }
+            >
+              {shiftInfo.event.extendedProps.overtime
+                ? "+" + shiftInfo.event.extendedProps.overtime.hours + "h"
+                : ""}
+            </span>
+          )}
+        </div>
       </div>
     </>
   );
